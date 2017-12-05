e03663a1a2d4badce74f927089199db4cbd4e6b6
Derek Miranda <derek.miranda@ff0000.com>
2017-12-05 12:16:56 -0800

    Schedule classes

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import DateManager from './lib/DateManager.js'
+import DateFormatter from './lib/DateFormatter.js'
+import Timezone from './lib/Timezone.js'
+import TzDate from './lib/TzDate.js'
+import RecurringSchedule from './lib/RecurringSchedule.js'
+import DateSchedule from './lib/DateSchedule.js'
 
 export {
 	DateManager,
+	DateFormatter,
+	Timezone,
+	TzDate,
+	RecurringSchedule,
+	DateSchedule,
 }
