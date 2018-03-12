e8ebbfad2dc4416d8b83072cd18ee634c2091c57
Kenny Arehart <kenny.arehart@ff0000.com>
2018-03-12 15:32:43 -0700

    added spanish as an export in the index

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-export { default as Timezone } from './lib/Timezone'
-export { default as TzDate } from './lib/TzDate'
-export { default as RecurringSchedule } from './lib/RecurringSchedule'
-export { default as DateSchedule } from './lib/DateSchedule'
+export { default as Timezone } from './lib/Timezone.js'
+export { default as TzDate } from './lib/TzDate.js'
+export { default as RecurringSchedule } from './lib/RecurringSchedule.js'
+export { default as DateSchedule } from './lib/DateSchedule.js'
+export { default as spanish } from './lib/languages/labelSpanish.js'
 
-import * as DateFormatter from './lib/DateFormatter'
-import * as DateManager from './lib/DateManager'
-import * as DateUtils from './lib/DateUtils'
+import * as DateFormatter from './lib/DateFormatter.js'
+import * as DateManager from './lib/DateManager.js'
+import * as DateUtils from './lib/DateUtils.js'
 export { DateFormatter, DateManager, DateUtils }
