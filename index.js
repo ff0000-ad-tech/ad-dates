bf2a9e9a3657b6b547d7fa6f44e713a77f5d8576
Kenny Arehart <kenny.arehart@ff0000.com>
2018-06-07 17:41:57 -0700

    changed DateManager to class to account for deafultTimezone export

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ export { default as RecurringSchedule } from './lib/RecurringSchedule.js'
 export { default as DateSchedule } from './lib/DateSchedule.js'
 export { default as spanish } from './lib/languages/labelSpanish.js'
 
+export { default as DateManager } from './lib/DateManager.js'
+
 import * as DateFormatter from './lib/DateFormatter.js'
-import * as DateManager from './lib/DateManager.js'
+// import * as DateManager from './lib/DateManager.js'
 import * as DateUtils from './lib/DateUtils.js'
 import * as Timezone from './lib/Timezone.js'
-export { DateFormatter, DateManager, DateUtils, Timezone }
+export { DateFormatter, DateUtils, Timezone }
