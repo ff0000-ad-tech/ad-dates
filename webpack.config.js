87c2532b24235bb28ed8167c0d09100a635ff24e
Kenny Arehart <kenny.arehart@ff0000.com>
2018-06-18 12:09:21 -0700

    added multi dist/ for with and w/o console

--- webpack.config.js
+++ webpack.config.js
@@ -18,52 +18,65 @@ const babelOptions = {
 	"plugins": [ "transform-class-properties" ]
 }
 
-module.exports = {
-	entry: path.resolve(__dirname, `./dist-entry.js`),
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'ad-dates.min.js'
-		// libraryTarget: 'umd'
-	},
-	resolve: {
-		alias: DM.aliases.getTopLevel(path.resolve(__dirname, 'node_modules/@ff0000-ad-tech'))
-	},
-	plugins: [
-		new UglifyJsPlugin({
-			uglifyOptions: {
-				drop_console: true
-			}
-		})
-	],
-	module: {
-		rules: [
-			{
-				test: request => {
-					log('test()', request.includes('ad-dates'), request.endsWith('index.js'), '|', request)
-					return request.includes('ad-dates') && request.endsWith('index.js')
-				},
-				use: [
-					{
-						loader: '@ff0000-ad-tech/webpack-rollup-babel-loader',
-						options: {
-							babelOptions: {
-								presets: babelOptions.presets
+module.exports = config => {
+	// if (config) {
+	// 	config = JSON.parse(config)
+	// } else {
+	// 	config = {}
+	// }
+
+	const isDebug = config.environment == 'debug'
+	const removeConsole = !isDebug
+	console.log('config:', config, '| isDebug:', isDebug, '| removeConsole:', removeConsole)
+
+	return {
+		entry: path.resolve(__dirname, `./dist-entry.js`),
+		output: {
+			path: path.resolve(__dirname, 'dist'),
+			filename: 'ad-dates.' + config.environment + '.js'
+			// libraryTarget: 'umd'
+		},
+		resolve: {
+			alias: DM.aliases.getTopLevel(path.resolve(__dirname, 'node_modules/@ff0000-ad-tech'))
+		},
+		plugins: [
+			new UglifyJsPlugin({
+				compress: {
+					drop_console: removeConsole,
+					warnings: removeConsole
+				}
+			})
+		],
+		module: {
+			rules: [
+				{
+					test: request => {
+						log('test()', request.includes('ad-dates'), request.endsWith('index.js'), '|', request)
+						return request.includes('ad-dates') && request.endsWith('index.js')
+					},
+					use: [
+						{
+							loader: '@ff0000-ad-tech/webpack-rollup-babel-loader',
+							options: {
+								babelOptions: {
+									presets: babelOptions.presets
+								}
 							}
 						}
-					}
-				]
-			},
-			{
-				test: /\.js$/,
-				use: [
-					{
-						loader: 'babel-loader',
-						options: {
-							plugins: babelOptions.plugins
+					]
+				},
+				{
+					test: /\.js$/,
+					use: [
+						{
+							loader: 'babel-loader',
+							options: {
+								plugins: babelOptions.plugins
+							}
 						}
-					}
-				]
-			}
-		]
+					]
+				}
+			]
+		}
 	}
 }
