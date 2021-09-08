4b9fed2f17da388607a50a94be8f1f9a6e69459a
Greg Connell <gconnell@160over90.com>
2021-09-07 17:19:49 -0700

    latest babel config

--- dist-entry.js
+++
@@ -1,5 +0,0 @@
-import * as adDates from './index.js'
-;['TzDate', 'RecurringSchedule', 'DateSchedule', 'DateFormatter', 'DateManager', 'DateUtils', 'Timezone'].forEach(function(key) {
-	window[key] = adDates[key]
-})
-window['DateFormatter'].addLanguage(adDates['spanish'])
